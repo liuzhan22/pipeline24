b39c141b65e045cdff0497eb51985cdaf983b8ea
liuzhan22 <liuzhan_22@outlook.com>
2024-07-11 23:55:46 +0800

    Step version 1: This version can run simple add instructions successfully

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -7,7 +7,9 @@ module InstructionMemory(
 		case (Address[9:2])
 
 			// -------- Paste Binary Instruction Below (Inst-q1-1/Inst-q1-2.txt)
-
+8'd0:	Instruction <= 32'h20090001;
+8'd1:	Instruction <= 32'h200a0002;
+8'd2:	Instruction <= 32'h200b0003;
 
 			// -------- Paste Binary Instruction Above
 			
